44533a796817a42f36db1f23d92878ec2759e411
zorg9pi9 <zorg99900000@mail.ru>
2017-12-20 17:35:20 +0100

    HammingDistance

---
+++ src/HammingDistance.java
@@ -0,0 +1,22 @@
+/**
+ * Created by zorg9 on 20.12.2017.
+ */
+public class HammingDistance {
+
+     static int solution(int x, int y) {
+         int val = x^y;
+         int count =0;
+
+         while(val!=0){
+             ++count;
+             val &=val-1;
+
+         }
+
+         return count;
+    }
+
+    public static void main (String[] args) {
+        System.out.print(solution(1,4));
+    }
+}
