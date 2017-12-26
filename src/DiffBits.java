32b370d0cc4a2aaf089c4398de08ced22ef2e3ef
zorg9pi9 <zorg99900000@mail.ru>
2017-12-26 16:33:23 +0100

    new classes

---
+++ src/DiffBits.java
@@ -0,0 +1,21 @@
+/**
+ * Created by zorg9 on 26.12.2017.
+ */
+public class DiffBits {
+
+    static int findComplement(int num) {
+        int i = num;
+        i |= (i >>  1);
+        i |= (i >>  2);
+        i |= (i >>  4);
+        i |= (i >>  8);
+        i |= (i >> 16);
+        return num ^ ((i & ~(i >> 1)) << 1) - 1;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(findComplement(5));
+
+    }
+}
---
+++ src/SumOfTwoArrays.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class SumOfTwoArrays {
+    static int[] solution(int[] a, int[] b) {
+        return (a != null && b != null ? sum(a, b) : (a == null ? b : a));
+    }
+
+    static int[] sum(int[] a, int[] b) {
+        int a1 = IntStream.range(0, a.length).reduce(0, (acc, x) -> acc + (int) Math.pow(10, x) * a[a.length - x - 1]);
+        int b1 = IntStream.range(0, b.length).reduce(0, (acc, y) -> acc + (int) Math.pow(10, y) * b[b.length - y - 1]);
+        return Integer.toString(a1 + b1).chars().map(c -> c -= '0').toArray();
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 5, 2};
+        int[] b = {4, 2, 6, 7};
+        System.out.print(Arrays.toString(solution(a, b)));
+    }
+}
