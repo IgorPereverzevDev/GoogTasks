39316a14cc781c7f3154bd65d45bccb059b7a8ca
zorg9pi9 <zorg99900000@mail.ru>
2018-07-26 10:45:45 +0200

    new classes

--- src/CheckTwoStringsOnSubstring.java
+++ src/CheckTwoStringsOnSubstring.java
@@ -1,4 +1,8 @@
+import java.util.Arrays;
+
 public class CheckTwoStringsOnSubstring {
+
+    static int MAX_CHAR = 26;
 
     public static boolean solution(String s1, String s2) {
         return (toInt(s1) & toInt(s2)) > 0;
@@ -13,6 +17,26 @@ public class CheckTwoStringsOnSubstring {
         }
         return res;
     }
+
+    static boolean twoStrings(String s1, String s2)
+    {
+        // vector for storing character occurrences
+        boolean v[]=new boolean[MAX_CHAR];
+        Arrays.fill(v,false);
+
+        // increment vector index for every
+        // character of str1
+        for (int i = 0; i < s1.length(); i++)
+            v[s1.charAt(i) - 'a'] = true;
+
+        // checking common substring of str2 in str1
+        for (int i = 0; i < s2.length(); i++)
+            if (v[s2.charAt(i) - 'a'])
+                return true;
+
+        return false;
+    }
+
 
     public static void main(String[] args) {
         System.out.println(CheckTwoStringsOnSubstring.solution("hi", "world"));
